9de2fd9c33820dfb076e1bff20666f7d8115774e
Shubham1740 <cse9528.shubham@gmail.com>
2024-04-14 19:37:26 +0530

    ADD: notifications

--- client/src/components/Notification.jsx
+++ client/src/components/Notification.jsx
@@ -1,28 +1,48 @@
 import { CaretRightOutlined } from '@ant-design/icons';
 import React from 'react';
 import { Collapse, theme } from 'antd';
-const text = `
-  A dog is a type of domesticated animal.
-  Known for its loyalty and faithfulness,
-  it can be found as a welcome guest in many households across the world.
-`;
+
+const Title = "Notificaiton1"
+const Timeline = "9:00 to 18:00"
+const Description = "water cuts in city for maintainence";
+const location = "sector 42 community center"
+const date = "From: 2024-04-14 To: 2024-04-15"
 const getItems = (panelStyle) => [
     {
         key: '1',
-        label: 'This is panel header 1',
-        children: <p>{text}</p>,
+        label: <h1 className='text-xl'>{Title}</h1>,
+        children: <div className='text-xl'>
+            <div className='font-bold flex justify-end' >
+                <h1 className='mx-1'>
+                    Timeline:
+                </h1>
+                {Timeline}
+            </div>
+            <p>
+                {Description}
+            </p>
+            <div className='font-bold' htmlFor="">
+                <h1>
+                Address:
+                </h1> 
+                {location}
+                </div>
+            <div className='flex justify-end'>
+                {date}
+            </div>
+        </div>,
         style: panelStyle,
     },
     {
         key: '2',
-        label: 'This is panel header 2',
-        children: <p>{text}</p>,
+        label: <h1 className='text-lg'>{Title}</h1>,
+        children: <div className='text-lg'><p>{Description}</p><label className='font-bold' >Timeline: </label>{Timeline}<br /> <label className='font-bold' htmlFor="">Address: </label> {location} <br />{date}</div>,
         style: panelStyle,
     },
     {
         key: '3',
-        label: 'This is panel header 3',
-        children: <p>{text}</p>,
+        label: <h1 className='text-lg'>{Title}</h1>,
+        children: <div className='text-lg'><p>{Description}</p><label className='font-bold' >Timeline: </label>{Timeline}<br /> <label className='font-bold' htmlFor="">Address: </label> {location} <br />{date}</div>,
         style: panelStyle,
     },
 ];
@@ -35,13 +55,14 @@ const App = () => {
         border: 'none',
     };
     return (
-        <div className='m-8 w-[60vw]'>
+        <div className='mt-8 flex justify-center'>
             <Collapse
                 bordered={false}
                 defaultActiveKey={['1']}
                 expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />}
                 style={{
                     background: "transparent",
+                    width: "70rem",
                 }}
                 items={getItems(panelStyle)}
             />
